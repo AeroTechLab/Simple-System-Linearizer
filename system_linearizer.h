7f1900b9572ebc1753573bd7af2042bef46c86f8
Bitiquinho <consoni_2519@hotmail.com>
2019-10-18 00:02:54 -0300

    File Sync

--- system_linearizer.h
+++ system_linearizer.h
@@ -1,3 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Copyright (c) 2019 Leonardo Consoni <leonardojc@protonmail.com>            //
+//                                                                             //
+//  This file is part of Simple-System-Linearizer.                             //
+//                                                                             //
+//  Simple-System-Linearizer is free software: you can redistribute it         //
+//  and/or modify it under the terms of the GNU Lesser General Public License  //
+//  as published by the Free Software Foundation, either version 3 of the      //
+//  License, or (at your option) any later version.                            //
+//                                                                             //
+//  Simple-System-Linearizer is distributed in the hope that it will           //
+//  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty     //
+//  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            //
+//  GNU Lesser General Public License for more details.                        //
+//                                                                             //
+//  You should have received a copy of the GNU Lesser General Public License   //
+//  along with Simple-System-Linearizer.                                       //
+//  If not, see <http://www.gnu.org/licenses/>.                                //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
 #ifndef SYSTEM_LINEARIZER_H
 #define SYSTEM_LINEARIZER_H
 
